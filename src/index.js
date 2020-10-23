9a6841e57f0f605ae59a40ae3daa6bf63a189d8f
irinaSk <irinaskid@gmail.com>
2020-10-23 22:44:41 +0300

    feat: add function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let n = str.length / 2;
+  do {
+    for (let i = 0; i < bracketsConfig.length; i++){
+      //пара "скобок"
+          let brackets = bracketsConfig[i][0] +  bracketsConfig[i][1];
+      //удаляем их из проверяемой строки
+          str = str.replace(brackets, '');
+      }
+      n--;
+  }
+  while (str !== '' && n > 0)   
+  if (str !== ''){
+    return false;
+  } else return true;
 }
